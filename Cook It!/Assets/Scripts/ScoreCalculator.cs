6eedd71e54fbee2725d9d50950035f5d05ab15af
JoshuaRiches <71353999+JoshuaRiches@users.noreply.github.com>
2021-11-11 10:18:46 +0000

    modified score calculations

--- Cook It!/Assets/Scripts/ScoreCalculator.cs
+++ Cook It!/Assets/Scripts/ScoreCalculator.cs
@@ -6,10 +6,14 @@ public class ScoreCalculator : MonoBehaviour
 {
     private float totalOrderScore;
     private float orderScore;
+    private float customerSatisfaction;
+    private float averageSatisfaction;
 
     private void Start()
     {
         totalOrderScore = 0;
+        orderScore = 0;
+        customerSatisfaction = 0;
     }
 
     public float CalculateIngredient(float a_baseScore, float a_errorMargin)
@@ -20,8 +24,10 @@ public class ScoreCalculator : MonoBehaviour
         return ingredScore;
     }
 
-    public float CalculateOrder(float[] a_ingredientScores, float a_timeTaken)
+    public float CalculateOrder(float[] a_ingredientScores, float a_timeTaken, float a_patience)
     {
+        orderScore = 0;
+
         for (int i = 0; i < a_ingredientScores.Length; i++)
         {
             orderScore += a_ingredientScores[i];
@@ -29,13 +35,16 @@ public class ScoreCalculator : MonoBehaviour
 
         orderScore = orderScore / a_timeTaken;
 
+        orderScore = orderScore * a_patience;
+
         return orderScore;
     }
 
     public float FinalScore(float[] a_orderScores, float a_lawsuitAmount, float a_lostMoney, float a_casualtyTotal, float a_leaverTotal)
     {
         float finalScore = 0;
-        
+
+        totalOrderScore = 0;
 
         for (int i = 0; i < a_orderScores.Length; i++)
         {
@@ -47,5 +56,31 @@ public class ScoreCalculator : MonoBehaviour
         finalScore -= a_leaverTotal * a_lostMoney;
 
         return finalScore;
+    }
+
+    public float CustomerSatisfaction(float[] a_errorMargins, float a_patience)
+    {
+        customerSatisfaction = 0;
+
+        for (int i = 0; i < a_errorMargins.Length; i++)
+        {
+            customerSatisfaction += a_errorMargins[i];
+        }
+
+        customerSatisfaction += a_patience;
+
+        return customerSatisfaction;
+    }
+
+    public float AverageSatisfaction(float[] a_satisfactions)
+    {
+        for (int i = 0; i < a_satisfactions.Length; i++)
+        {
+            averageSatisfaction += a_satisfactions[i];
+        }
+
+        averageSatisfaction = averageSatisfaction / a_satisfactions.Length;
+
+        return averageSatisfaction;
     }
 }
